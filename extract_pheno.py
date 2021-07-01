b6c9851a1845edd8601b6063b5de09758d685f94
JFreud <jrm.frdnbrg@gmail.com>
2021-07-01 16:07:18 -0500

    outputs files into dir + optional target dir flag + search by largest - smallest dataset ID

--- extract_pheno.py
+++ extract_pheno.py
@@ -1,8 +1,9 @@
 import numpy as np
 import argparse
+import re
 from pandas import read_csv
 from glob import glob
-from os import stat, getenv, path
+from os import stat, getenv, path, makedirs
 from dotenv import load_dotenv
 from sys import argv, stderr, exit
 
@@ -26,9 +27,9 @@ def dir_path(string):
     if path.isdir(string):
         return string
     else:
-        raise argparse.ArgumentTypeError(f"readable_dir:{path} is not a valid path")
+        raise argparse.ArgumentTypeError(f"directory: {string} is not a valid directory path")
 
-def extract_phenotypes(ID_list, output_fname, combine=False, combine_op="last", chunksize=10000, pheno_dir=None, debug=False):
+def extract_phenotypes(ID_list, output_fname, combine=False, combine_op="last", chunksize=10000, pheno_dir=None, target_dir=".", debug=False):
     """Extract phenotypes from ukb csv and write to plink friendly format
 
     Args:
@@ -40,9 +41,10 @@ def extract_phenotypes(ID_list, output_fname, combine=False, combine_op="last", 
         bool: Returns True on success, False on failure
     """
 
-    # grab files matching ukb*.csv for IDs in order of smallest to largest
+    # grab files matching ukb*.csv for IDs in order of largest dataset ID to smallest e.g. ukb27702.csv is searched before ukb27701.csv
     pheno_dir = path.join(path.abspath(pheno_dir), '') + 'ukb*.csv' if pheno_dir else path.expanduser(getenv('PHENO_PATH') + 'ukb*.csv')
-    file_list = sorted(glob(pheno_dir), key=lambda x: stat(x).st_size)
+    file_list = sorted(glob(pheno_dir), key=lambda x: int(path.basename(x).split('.')[0].replace('ukb', '')), reverse=True)
+    # file_list = sorted(glob(pheno_dir), key=lambda x: stat(x).st_size) # <-- sort by smallest file size to largest
     if debug:
         print("Pheno dir: ", pheno_dir)
         print("File list:", file_list)
@@ -50,21 +52,23 @@ def extract_phenotypes(ID_list, output_fname, combine=False, combine_op="last", 
     # map filename to included field IDs
     ID_list = set(ID_list)
     fname_index = 0
+    # create output dir
+    makedirs(path.join(path.abspath(target_dir), '') + output_fname, exist_ok=True)
     while ID_list and fname_index < len(file_list):
         filename = file_list[fname_index]
         header = get_header(filename, combine)
         header_names = set(header.unique())
         
         if debug:
-            print(filename)
-            print(header)
-            print(header_names)
+            print("File being read: ", filename)
+            print("File header: ", header)
+            print("Unique header names: ", header_names)
 
         # if file contains any of the phenotypes, read that column and write to plink format pheno file
         included_cols = list(ID_list.intersection(header_names))
         if debug:
             print("Included cols: ", included_cols)
-        # TODO: Create output dir
+        
         if len(included_cols) > 0:
             usecols = lambda x: x == "eid" or any(field_id in x for field_id in included_cols)
             with read_csv(filename, usecols=usecols, chunksize=chunksize, dtype=str) as reader:
@@ -85,8 +89,9 @@ def extract_phenotypes(ID_list, output_fname, combine=False, combine_op="last", 
                     chunk.insert(loc=0, column="FID", value=0)
                     chunk.fillna(value=-9, inplace=True)
                     if debug:
-                        print(chunk)
-                    chunk.to_csv(output_fname + get_dataset_id(filename) + ".pheno", header=is_header, index=False, sep=' ', mode=mode)
+                        print("Chunk to be written:\n", chunk)
+                    target_file = path.join(path.abspath(target_dir), '') + output_fname + "/" + output_fname + get_dataset_id(filename) + ".pheno"
+                    chunk.to_csv(target_file, header=is_header, index=False, sep=' ', mode=mode)
                     print("Wrote ", output_fname + get_dataset_id(filename) + ".pheno")
                     is_header=0
                     mode = 'a'
@@ -102,10 +107,6 @@ def extract_phenotypes(ID_list, output_fname, combine=False, combine_op="last", 
 
 
 if __name__ == "__main__":
-    # test call
-    # extract_phenotypes(["21","31","34","44","45","50","72"], "test", combine=True, chunksize=10000, debug=True)
-    # extract_phenotypes(["31-0.0","34-0.0","44-0.0","50-0.0","50-1.0"], "rip", combine=False, chunksize=10000, debug=True)
-
     # parse arguments
     class CustomFormatter(argparse.RawTextHelpFormatter, argparse.RawDescriptionHelpFormatter):
         pass
@@ -119,30 +120,31 @@ if __name__ == "__main__":
     parser.add_argument("fields", nargs="+", help="""UKB field IDs or full column IDs to be extracted e.g.
     "31-?.? 31-?.? 50-0.0 50-1.0 50-2.0 1299-0.0" if you want to keep instance/arrays separate
     "31 50 1299" if you want to take the latest measured instance. Requires combine flag.""")
-    parser.add_argument("-o", "--output", help="Output directory name e.g. 'foo'. This will create directory foo/ with files foo[datasetID1].pheno, foo[datasetID2].pheno etc", 
+    parser.add_argument("-n", "--name", help="Output directory name e.g. 'foo'. This will create directory foo/ with files foo[datasetID1].pheno, foo[datasetID2].pheno etc", 
                         required=True)
     parser.add_argument("-c", "--combine", nargs="?", help="Flag to combine columns with same field ids. Options are 'none', 'last', 'first'. Default is to take most recent non-empty value (rightmost in the csv).", 
                         default="none", const="last")
     # TODO: Better help for combine flag
-    parser.add_argument("-r", "--rows", help="Number of rows to read in at a time when parsing the csv. Default is 10000.", default=10000)
+    parser.add_argument("-r", "--rows", help="Number of rows to read in at a time when parsing the csv. Default is 10000.", type=int, default=10000)
     parser.add_argument("-d", "--dir", help="Directory to look for ukb files. Defaults to path in .env file", type=dir_path, action="store")
+    parser.add_argument("-t", "--target", help="Directory to place extracted phenotypes folder. Default is current directory.", type=dir_path, action="store", default=".")
     parser.add_argument("-v", "--verbose", help="Print intermediate outputs for debugging", action="store_true", default=False)
 
     args = vars(parser.parse_args())
     ID_list = args["fields"]
-    output_fname = args["output"]
+    output_fname = args["name"]
     combine_op = args["combine"]
     combine = False if combine_op == "none" else True
     chunksize = int(args["rows"])
     debug = args["verbose"]
     pheno_dir = args["dir"]
-    print(pheno_dir)
+    target_dir = args["target"]
 
     print("Extracting phenotypes...")
-    extract_phenotypes(ID_list, output_fname, combine, combine_op, chunksize, pheno_dir, debug)
+    extract_phenotypes(ID_list, output_fname, combine, combine_op, chunksize, pheno_dir, target_dir, debug)
 
     # test commands (reads ukb*.csv in test directory)
-    # python3 extract_pheno.py 21 31 34 44 45 50 72 -o comm -c -r 100 -d tests -v
-    # python3 extract_pheno.py 31-0.0 34-0.0 44-0.0 50-0.0 50-1.0 50-2.0 -o nocom  -r 100 -d tests -v
+    # python3 extract_pheno.py 21 31 34 44 45 50 72 -n comb -c -r 100 -d tests -t dummy -v
+    # python3 extract_pheno.py 31-0.0 34-0.0 44-0.0 50-0.0 50-1.0 50-2.0 -n nocom -r 100 -d tests -t dummy -v
 
 
