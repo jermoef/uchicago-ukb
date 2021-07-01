a794fe7f518a4465ca14738759423de27a424bbf
JFreud <jrm.frdnbrg@gmail.com>
2021-07-01 17:51:59 -0500

    exclude samples from exclusion file

--- extract_pheno.py
+++ extract_pheno.py
@@ -1,3 +1,4 @@
+from typing import DefaultDict
 import numpy as np
 import argparse
 import re
@@ -29,7 +30,7 @@ def dir_path(string):
     else:
         raise argparse.ArgumentTypeError(f"directory: {string} is not a valid directory path")
 
-def extract_phenotypes(ID_list, output_fname, combine=False, combine_op="last", chunksize=10000, pheno_dir=None, target_dir=".", debug=False):
+def extract_phenotypes(ID_list, output_fname, combine=False, combine_op="last", chunksize=10000, pheno_dir=None, target_dir=".", exclude_file=None, debug=False):
     """Extract phenotypes from ukb csv and write to plink friendly format
 
     Args:
@@ -44,7 +45,10 @@ def extract_phenotypes(ID_list, output_fname, combine=False, combine_op="last", 
     # grab files matching ukb*.csv for IDs in order of largest dataset ID to smallest e.g. ukb27702.csv is searched before ukb27701.csv
     pheno_dir = path.join(path.abspath(pheno_dir), '') + 'ukb*.csv' if pheno_dir else path.expanduser(getenv('PHENO_PATH') + 'ukb*.csv')
     file_list = sorted(glob(pheno_dir), key=lambda x: int(path.basename(x).split('.')[0].replace('ukb', '')), reverse=True)
-    # file_list = sorted(glob(pheno_dir), key=lambda x: stat(x).st_size) # <-- sort by smallest file size to largest
+    # grab list of UKB participants who have withdrawn and should be excluded
+    exclude_file = exclude_file if exclude_file else getenv('EXCLUSION_FILE')
+    exclusion_index = read_csv(exclude_file, header=None, index_col=0).index
+
     if debug:
         print("Pheno dir: ", pheno_dir)
         print("File list:", file_list)
@@ -54,6 +58,7 @@ def extract_phenotypes(ID_list, output_fname, combine=False, combine_op="last", 
     fname_index = 0
     # create output dir
     makedirs(path.join(path.abspath(target_dir), '') + output_fname, exist_ok=True)
+
     while ID_list and fname_index < len(file_list):
         filename = file_list[fname_index]
         header = get_header(filename, combine)
@@ -71,7 +76,7 @@ def extract_phenotypes(ID_list, output_fname, combine=False, combine_op="last", 
         
         if len(included_cols) > 0:
             usecols = lambda x: x == "eid" or any(field_id in x for field_id in included_cols)
-            with read_csv(filename, usecols=usecols, chunksize=chunksize, dtype=str) as reader:
+            with read_csv(filename, usecols=usecols, chunksize=chunksize, dtype=str, index_col="eid") as reader:
                 is_header = 1
                 mode = 'w'
                 for chunk in reader:
@@ -83,9 +88,9 @@ def extract_phenotypes(ID_list, output_fname, combine=False, combine_op="last", 
                         else:
                             chunk = chunk.groupby(level=0, axis=1).last()
                     
+                    chunk = chunk[~chunk.index.isin(exclusion_index)] # drop excluded samples
                     # set FID = 0, IID = eid in first two columns, fill NA values with -9
-                    eid_col = chunk.pop("eid")
-                    chunk.insert(loc=0, column="IID", value=eid_col)
+                    chunk.insert(loc=0, column="IID", value=chunk.index)
                     chunk.insert(loc=0, column="FID", value=0)
                     chunk.fillna(value=-9, inplace=True)
                     if debug:
@@ -93,13 +98,12 @@ def extract_phenotypes(ID_list, output_fname, combine=False, combine_op="last", 
                     target_file = path.join(path.abspath(target_dir), '') + output_fname + "/" + output_fname + get_dataset_id(filename) + ".pheno"
                     chunk.to_csv(target_file, header=is_header, index=False, sep=' ', mode=mode)
                     print("Wrote ", output_fname + get_dataset_id(filename) + ".pheno")
-                    is_header=0
+                    is_header = 0
                     mode = 'a'
 
         ID_list = ID_list.difference(header_names)
         fname_index += 1
 
-    # TODO: Throw out exclusion list
     if ID_list:
         print("The following IDs were not found in the UKB csv files: ", ID_list)
         return False
@@ -128,6 +132,7 @@ if __name__ == "__main__":
     parser.add_argument("-r", "--rows", help="Number of rows to read in at a time when parsing the csv. Default is 10000.", type=int, default=10000)
     parser.add_argument("-d", "--dir", help="Directory to look for ukb files. Defaults to path in .env file", type=dir_path, action="store")
     parser.add_argument("-t", "--target", help="Directory to place extracted phenotypes folder. Default is current directory.", type=dir_path, action="store", default=".")
+    parser.add_argument("-e", "--exclude", help="CSV where first column is list of IDs to exclude in the output phenotype files")
     parser.add_argument("-v", "--verbose", help="Print intermediate outputs for debugging", action="store_true", default=False)
 
     args = vars(parser.parse_args())
@@ -139,12 +144,13 @@ if __name__ == "__main__":
     debug = args["verbose"]
     pheno_dir = args["dir"]
     target_dir = args["target"]
+    exclude_file = args["exclude"]
 
     print("Extracting phenotypes...")
-    extract_phenotypes(ID_list, output_fname, combine, combine_op, chunksize, pheno_dir, target_dir, debug)
+    extract_phenotypes(ID_list, output_fname, combine, combine_op, chunksize, pheno_dir, target_dir, exclude_file, debug)
 
     # test commands (reads ukb*.csv in test directory)
-    # python3 extract_pheno.py 21 31 34 44 45 50 72 -n comb -c -r 100 -d tests -t dummy -v
-    # python3 extract_pheno.py 31-0.0 34-0.0 44-0.0 50-0.0 50-1.0 50-2.0 -n nocom -r 100 -d tests -t dummy -v
+    # python3 extract_pheno.py 21 31 34 44 45 50 72 -n comb -c -r 100 -d tests -e tests/test_exclude.csv -v
+    # python3 extract_pheno.py 31-0.0 34-0.0 44-0.0 50-0.0 50-1.0 50-2.0 -n nocom -r 100 -d tests -e tests/test_exclude.csv -v
 
 
