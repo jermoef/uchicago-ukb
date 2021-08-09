ebfe7ae957fcce8ae70bd65996c7b695c95c028f
JFreud <jrm.frdnbrg@gmail.com>
2021-08-09 13:05:03 -0500

    update exclusion file

--- consts.py
+++ consts.py
@@ -1,2 +1,2 @@
 PHENO_PATH = "/gpfs/data/ukb-share/phenotypes/phenotypes_decompressed/"
-EXCLUSION_FILE = "/gpfs/data/ukb-share/w19526_20210201.csv"
+EXCLUSION_FILE = "/gpfs/data/ukb-share/w19526_20210809.csv"
--- scripts/make_pgen_sequential.sh
+++ scripts/make_pgen_sequential.sh
@@ -2,11 +2,11 @@
 
 #PBS -N bgen2pgen
 #PBS -S /bin/bash
-#PBS -l walltime=64:00:00
+#PBS -l walltime=72:00:00
 #PBS -l nodes=1:ppn=28
 #PBS -l mem=16gb
-#PBS -o $HOME/plink_pgen.out
-#PBS -e $HOME/plink_pgen.err
+#PBS -o $HOME/plink2pgen.out
+#PBS -e $HOME/plink2pgen.err
 
 
 IN_FILE="/gpfs/data/ukb-share/genotypes/v3/ukb_imp_chr${chr_num}_v3.bgen"
@@ -17,30 +17,37 @@ module load gcc/6.2.0 parallel/20170122
 
 cd $OUT_DIR
 
-mkdir plink_genotypes
+mkdir -p plink_genotypes
 
 cd plink_genotypes
 
-env_parallel -j 3 ~/plink2 --bgen /gpfs/data/ukb-share/genotypes/v3/ukb_imp_chr{1}_v3.bgen ref-first \
-	 --sample $SAMPLE_FILE \
+for i in $(seq 1 22); do
+    sem -j 3 ~/plink2 --bgen /gpfs/data/ukb-share/genotypes/v3/ukb_imp_chr${i}_v3.bgen ref-first \
+	 --sample /gpfs/data/ukb-share/genotypes/ukb19526_imp_chr1_v3_s487395.sample \
 	 --export bcf \
-	 --out ukb_imp_chr{1}_v3 \
+	 --out ukb_imp_chr${i}_v3 \
 	 --memory 15000 \
-	 --threads 9 ::: {1..22}
+	 --threads 9
+done
+sem --wait
 
 : > merge.txt
 for i in $(seq 1 22); do
     echo 'ukb_imp_chr${i}_v3.bcf' >> merge.txt
 done
 
-~/bin/bcftools concat -f merge.txt -o ukb_imp_v3.bcf -O b --threads 27
+sem -j 1 ~/bin/bcftools concat -f merge.txt -o ukb_imp_v3.bcf -O b --threads 27
+
+sem --wait
 
 rm ukb_imp_chr*_v3.bcf
 
-~/plink2 --bcf ukb_imp_v3.bcf \
-	 --make-pgen \
-	 --out ukb_imp_v3 \
-	 --memory 15000 \
-	 --threads 27
+sem -j 1 ~/plink2 --bcf ukb_imp_v3.bcf \
+    --make-pgen \
+    --out ukb_imp_v3 \
+    --memory 15000 \
+    --threads 27
+
+sem --wait
 
 rm ukb_imp_v3.bcf
